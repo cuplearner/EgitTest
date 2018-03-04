92df577796789822a59fede38c2bffe21c0d7917
cuplearner <cuplearner@163.com>
2018-03-04 16:39:20 +0800

    新增123123

--- src/egittest123/EgitTest1.java
+++ src/egittest123/EgitTest1.java
@@ -4,7 +4,7 @@ public class EgitTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("githubtest");
+		System.out.println("githubtest123123");
 	}
 
 }
